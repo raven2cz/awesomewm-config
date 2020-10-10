5a1f393b22d41671092c8fb0aee91dd4471c1ab0
Antonin Fischer <tonda.fischer@gmail.com>
2020-10-10 10:03:02 +0200

    add synology and clipmenud deamon to the autorun process.

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -12,12 +12,13 @@ run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run /usr/lib/pam_kwallet_init
 run nm-applet
 run pamac-tray
+synology-drive start
 run alttab -mk Control_L -pk h -nk l -fg "#d58681" -bg "#4a4a4a" -frame "#eb564d" -t 128x150 -i 127x64
 run copyq
-run xautolock -time 3 -locker blurlock
+run xautolock -time 60 -locker blurlock -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
 run clipit
+run clipmenud
 run volumeicon
 run conky -c ~/.config/conky
 run remmina -i
 run picom --experimental-backends --config $HOME/.config/picom/picom.conf
-
