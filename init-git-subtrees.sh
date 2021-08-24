12fdd5e599521835ee9b0eb8f9324998b2bc8b41
Antonin Fischer <tonda.fischer@gmail.com>
2021-08-24 23:19:47 +0200

    Add git subtrees of the depended projects.

--- init-git-subtrees.sh
+++ init-git-subtrees.sh
@@ -5,4 +5,4 @@ git subtree add --prefix .config/awesome/lain git@github.com:lcpz/lain.git maste
 git subtree add --prefix .config/awesome/layout-machi git@github.com:xinhaoyuan/layout-machi.git master --squash
 git subtree add --prefix .config/awesome/machina git@github.com:basaran/awesomewm-machina.git master --squash
 git subtree add --prefix .config/awesome/nice git@github.com:mut-ex/awesome-wm-nice.git master --squash
-git subtree add --prefix .config/awesome/sharedtags git@github.com:Drauthius/awesome-sharedtags.git master --squash
+git subtree add --prefix .config/awesome/sharedtags git@github.com:Drauthius/awesome-sharedtags.git v4.0 --squash
