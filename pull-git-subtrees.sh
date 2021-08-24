3ef21278423c4feebb27346d364c727822f1f53c
Antonin Fischer <tonda.fischer@gmail.com>
2021-08-24 23:33:24 +0200

    Add script for fast pull of all git subtrees. Fast update of subprojects.

---
+++ pull-git-subtrees.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git subtree pull --prefix .config/awesome/awesome-wm-widgets git@github.com:streetturtle/awesome-wm-widgets.git master --squash
+git subtree pull --prefix .config/awesome/cyclefocus git@github.com:blueyed/awesome-cyclefocus.git master --squash
+git subtree pull --prefix .config/awesome/lain git@github.com:lcpz/lain.git master --squash
+git subtree pull --prefix .config/awesome/layout-machi git@github.com:xinhaoyuan/layout-machi.git master --squash
+git subtree pull --prefix .config/awesome/machina git@github.com:basaran/awesomewm-machina.git master --squash
+git subtree pull --prefix .config/awesome/nice git@github.com:mut-ex/awesome-wm-nice.git master --squash
+git subtree pull --prefix .config/awesome/sharedtags git@github.com:Drauthius/awesome-sharedtags.git v4.0 --squash
