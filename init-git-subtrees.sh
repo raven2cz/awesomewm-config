a2523ed8a710758507660930014cdc9cf466f81c
Antonin Fischer <tonda.fischer@gmail.com>
2021-08-29 09:25:36 +0200

    Make git subtree from AwesomeWM-Config project. This subtree is a part of raven2cz/dotfiles project now -> check this project for more information.

--- init-git-subtrees.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git subtree add --prefix .config/awesome/awesome-wm-widgets git@github.com:streetturtle/awesome-wm-widgets.git master --squash
-git subtree add --prefix .config/awesome/cyclefocus git@github.com:blueyed/awesome-cyclefocus.git master --squash
-git subtree add --prefix .config/awesome/lain git@github.com:lcpz/lain.git master --squash
-git subtree add --prefix .config/awesome/layout-machi git@github.com:xinhaoyuan/layout-machi.git master --squash
-git subtree add --prefix .config/awesome/machina git@github.com:basaran/awesomewm-machina.git master --squash
-git subtree add --prefix .config/awesome/nice git@github.com:mut-ex/awesome-wm-nice.git master --squash
-git subtree add --prefix .config/awesome/sharedtags git@github.com:Drauthius/awesome-sharedtags.git v4.0 --squash
