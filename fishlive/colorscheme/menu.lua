0871bd21a950b189a936ddf0d3fcdde29e4e25ea
Antonin Fischer <tonda.fischer@gmail.com>
2022-01-22 11:18:44 +0100

    Add conky theming.

--- fishlive/colorscheme/menu.lua
+++ fishlive/colorscheme/menu.lua
@@ -2,17 +2,36 @@ local colorscheme = require "fishlive.colorscheme"
 
 menu = {}
 
+local function writeToFile(resource, content)
+  local file = assert(io.open(resource, "w"))
+  file:write(content)
+  file:close()
+end
+
 -- Colorschemes Switcher --
 menu.prepare_colorscheme_menu = function()
   local menucs = {}
   for i, cs in ipairs(colorscheme.table) do
       menucs[i] = { cs.scheme, function()
+          local homeDir = os.getenv("HOME")
           -- call global colorscheme script for switch all GNU/Linux apps
-          os.execute("global-colorscheme.lua " .. cs.scheme_id)
+          os.execute(homeDir .. "/bin/global-colorscheme.lua " .. cs.scheme_id)
           -- permanent storage of selected colorscheme to last.lua
-          local file = io.open(os.getenv("HOME") .. "/.config/awesome/fishlive/colorscheme/last.lua", "w")
-          file:write('return require "fishlive.colorscheme".' .. cs.scheme_id)
-          file:close()
+          writeToFile(
+            homeDir .. "/.config/awesome/fishlive/colorscheme/last.lua",
+            'return require "fishlive.colorscheme".' .. cs.scheme_id
+          )
+          -- change rofi theme
+          writeToFile(
+            homeDir .. "/.config/rofi/config.rasi",
+            '@theme "'..homeDir..'/.config/rofi/multicolor-'..cs.scheme_id..'.rasi"'
+          )
+          -- change conky theme
+          writeToFile(
+            homeDir .. "/.config/conky/MX-CoreBlue/conkytheme.lua",
+            "return { color0 = '"..cs.base07.."', color1 = '" .. cs.base0A .. "' }"
+          )
+          -- restart AWESOME
           awesome.restart()
         end
       }
