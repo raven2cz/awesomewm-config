cb2be41a35cbfd0d60e50f6a23e409ab6532a6fd
Antonin Fischer <tonda.fischer@gmail.com>
2022-12-23 14:28:24 +0100

    Improve Dracule color theme by Zeno.

--- fishlive/colorscheme/dracula.lua
+++ fishlive/colorscheme/dracula.lua
@@ -5,26 +5,28 @@ local theme = {}
 -- {{{ Colors
 theme.scheme = "Dracula"
 theme.scheme_id = "dracula"
-theme.scheme_author = "Mike Barkmin (http =//github.com/mikebarkmin) theme based on Dracula Theme (http =//github.com/dracula)"
+theme.scheme_author = "Zeno Rocha (https://draculatheme.com"
 theme.scheme_type = "dark"
 theme.base00 = "#282936"
-theme.base01 = "#3a3c4e"
+theme.base01 = "#454158"
 theme.base02 = "#4d4f68"
 theme.base03 = "#626483"
-theme.base04 = "#62d6e8"
+theme.base04 = "#f8f8f2"
 theme.base05 = "#e9e9f4"
 theme.base06 = "#f1f2f8"
 theme.base07 = "#f7f7fb"
-theme.base08 = "#ea51b2"
-theme.base09 = "#b45bcf"
-theme.base0A = "#00f769"
-theme.base0B = "#ebff87"
-theme.base0C = "#a1efe4"
-theme.base0D = "#62d6e8"
-theme.base0E = "#b45bcf"
-theme.base0F = "#00f769"
+theme.base08 = "#ff9580" --# red
+theme.base09 = "#ffaa99" --# orange
+theme.base0A = "#ffff80" --# yellow
+theme.base0B = "#8aff80" --# green
+theme.base0C = "#80ffea" --# cyan
+theme.base0D = "#9580ff" --# blue
+theme.base0E = "#ff80bf" --# purple
+theme.base0F = "#aa99ff" --# brown
 
-theme.leading_fg = theme.base0A
+theme.fg_focus   = theme.base0B
+theme.leading_fg = theme.base0F
+theme.border_color_active = theme.base0B
 -- }}}
 
 return theme
